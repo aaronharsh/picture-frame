d41c3d9c7cfb5e8d357e0a7e5a7b47c2c237687c
Aaron Harsh <aaron.harsh@gmail.com>
2020-11-15 17:48:49 -0800

    First round of code

---
+++ images.py
@@ -0,0 +1,89 @@
+import io
+import json
+import os
+import pygame
+import random
+import requests
+import sys
+
+from config import config
+
+
+def get_remote_manifest(base_url):
+    response = requests.get(base_url + "/manifest.txt")
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+
+def get_remote_image_urls():
+    base_url = config["image_base_url"]
+
+    manifest = get_remote_manifest(base_url)
+    if manifest == None:
+        return None
+
+    image_urls = []
+
+    for image_basename in manifest.split("\n"):
+        if image_basename == "":
+            continue
+
+        image_urls.append(base_url + "/" + image_basename)
+
+    return image_urls
+
+
+def resize_image(content):
+    original_image = pygame.image.load(io.BytesIO(content))
+    original_size = original_image.get_size()
+    screen_size = (config["resolution_x"], config["resolution_y"])
+
+    x_ratio = screen_size[0] / original_size[0]
+    y_ratio = screen_size[1] / original_size[1]
+
+    ratio = min([x_ratio, y_ratio])
+
+    new_size = (round(original_size[0] * ratio), round(original_size[1] * ratio))
+
+    return pygame.transform.smoothscale(original_image, new_size)
+
+
+def sync_images():
+    cache_directory = config["cache_directory"]
+    if not os.path.exists(cache_directory):
+        try:
+            os.mkdir(cache_directory)
+        except Exception as e:
+            print(f"couldn't create {cache_directory}: e")
+            return
+
+    remote_image_urls = get_remote_image_urls()
+    if not remote_image_urls:
+        print(f"no remote image urls.  not syncing")
+        return
+
+    for image_url in remote_image_urls:
+        image_basename = os.path.basename(image_url)
+        local_path = cache_directory + "/" + image_basename
+        if os.path.exists(local_path):
+            print(f"{local_path} already exists - not downloading {image_url}")
+        else:
+            print(f"{local_path} doesn't exist - downloading {image_url}")
+            response = requests.get(image_url)
+            if response.status_code == 200:
+                image = resize_image(response.content)
+                pygame.image.save(image, local_path)
+            else:
+               print(f"failed to download {image_url}: {response.status_code}")
+
+
+def random_image():
+    cache_directory = config["cache_directory"]
+    files = os.listdir(cache_directory)
+    if not files:
+        return None
+
+    file = random.choice(files)
+    return pygame.image.load(cache_directory + "/" + file)
---
+++ run.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import pygame, sys
+from pygame.locals import *
+
+import images
+
+from config import config
+
+BLACK = (0, 0, 0)
+RESOLUTION_X = config["resolution_x"]
+RESOLUTION_Y = config["resolution_y"]
+
+
+def show_random_image():
+    image = images.random_image()
+
+    image_size = image.get_size()
+
+    left = round((RESOLUTION_X - image_size[0]) / 2)
+    top = round((RESOLUTION_Y - image_size[1]) / 2)
+
+    if image:
+        DISPLAYSURF.fill(BLACK)
+        DISPLAYSURF.blit(image, (left, top))
+        pygame.display.update()
+
+images.sync_images()
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((RESOLUTION_X, RESOLUTION_Y), pygame.FULLSCREEN)
+pygame.mouse.set_visible(False)
+
+show_random_image()
+
+mainLoop = True
+
+while mainLoop:
+    event = pygame.event.wait(config["refresh_pictures_time_seconds"] * 1000)
+
+    print(f"event = {event}")
+
+    if event.type == pygame.NOEVENT:
+        images.sync_images()
+
+    if event.type in [pygame.NOEVENT, pygame.KEYUP]:
+        show_random_image()
+
+    if event.type in [pygame.QUIT, pygame.MOUSEBUTTONUP]:
+        mainLoop = False
+
+    pygame.display.update()
+
+pygame.quit()
