12aac723d91f63556b341cb6f9fd9b165697e4d2
Aaron Harsh <aaron.harsh@gmail.com>
2020-11-16 19:36:06 -0800

    gamma correction

--- images.py
+++ images.py
@@ -11,6 +11,9 @@ from config import config
 
 cache_directory = config["cache_directory"]
 
+gamma = config["gamma"]
+gamma_table = [round(((i/255.0) ** gamma) * 255) for i in range(256)]
+
 
 def get_remote_manifest(base_url):
     response = requests.get(base_url + "/manifest.txt")
@@ -51,9 +54,18 @@ def fetch_image(image_url):
        print(f"failed to download {image_url}: {response.status_code}")
 
 
-def resize_image(original_path, resized_path):
-    original_image = pygame.image.load(original_path)
-    original_size = original_image.get_size()
+def gamma_correct(image):
+    gamma = config["gamma"]
+
+    image_bytes = bytearray(pygame.image.tostring(image, "RGB"))
+    for i in range(len(image_bytes)):
+        image_bytes[i] = gamma_table[image_bytes[i]]
+
+    return pygame.image.fromstring(bytes(image_bytes), image.get_size(), "RGB")
+
+
+def resize_image(image):
+    original_size = image.get_size()
     screen_size = (config["resolution_x"], config["resolution_y"])
 
     x_ratio = screen_size[0] / original_size[0]
@@ -62,12 +74,10 @@ def resize_image(original_path, resized_path):
     ratio = min([x_ratio, y_ratio])
 
     new_size = (round(original_size[0] * ratio), round(original_size[1] * ratio))
-    resized = pygame.transform.smoothscale(original_image, new_size)
-
-    pygame.image.save(resized, resized_path)
+    return pygame.transform.smoothscale(image, new_size)
 
 
-def fetch_and_resize_image(image_url):
+def fetch_and_prepare_image(image_url):
     image_basename = os.path.basename(image_url)
 
     local_path = cache_directory + "/" + image_basename
@@ -75,9 +85,13 @@ def fetch_and_resize_image(image_url):
     if os.path.exists(local_path):
         print(f"{local_path} already exists - not downloading {image_url}")
     else:
-        unresized_path = fetch_image(image_url)
-        resize_image(unresized_path, local_path)
-        os.unlink(unresized_path)
+        original_path = fetch_image(image_url)
+        image = pygame.image.load(original_path)
+        image = gamma_correct(image)
+        image = resize_image(image)
+
+        pygame.image.save(image, local_path)
+        os.unlink(original_path)
 
 
 def sync_images():
@@ -95,7 +109,7 @@ def sync_images():
             return
 
         for image_url in remote_image_urls:
-            fetch_and_resize_image(image_url)
+            fetch_and_prepare_image(image_url)
 
         remote_image_basenames = [os.path.basename(u) for u in remote_image_urls]
 
