5193553d676a04f2d64feb12c9952b94b71892ec
Aaron Harsh <aaron.harsh@gmail.com>
2020-11-28 17:02:40 -0800

    checkpoint

--- start-picture-frame.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-(
-    echo Started $$ at $( date '+Y-%m-%d %H:%M:%S' ) > /tmp/picture-frame.pid
-    cd ~/picture-frame
-    git pull
-    source .venv/bin/activate
-    python run.py
-) &> /tmp/picture-frame.log
